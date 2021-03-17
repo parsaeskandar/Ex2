32382c30513c83064d12a514bd335a730ebde3d9
parsa <parsa.eskandar@gmail.com>
2021-03-17 23:16:51 +0330

    add change width and height to rectangle class

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -17,4 +17,26 @@ public class Rectangle implements Shape {
     public double computeArea() {
         return width * height;
     }
+    
+    public void setWidth(double width) {
+        this.width = 0;
+        if (width >= 0) {
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height) {
+        this.height = 0;
+        if (height >= 0) {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
 }
