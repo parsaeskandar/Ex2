14047be520d140c62b96dd8f0e49063b0f1ed4c8
Kim <yazdanikimia@yahoo.com>
2021-03-17 23:11:54 +0330

    added new unit tests

--- src/UnitTest.java
+++ src/UnitTest.java
@@ -23,6 +23,34 @@ public class UnitTest {
         assertEquals(r.computeArea(), 8, 0.0001);
     }
 
+
+    @Test
+    public void test4() {
+        Rectangle r = new Rectangle(7, 2);
+        r.setHeight(-2);
+        assertEquals(r.computeArea(), 0, 0.0001);
+    }
+
+    @Test
+    public void test5() {
+        Square s = new Square(5);
+        assertEquals(s.computeArea(), 25, 0.0001);
+    }
+
+    @Test
+    public void test6() {
+        Square s = new Square(5);
+        s.setHeight(-3);
+        assertEquals(s.computeArea(), 0, 0.0001);
+    }
+
+
+    @Test
+    public void test7() {
+        Square s = new Square(2);
+        s.setHeight(2.5);
+        assertEquals(s.computeArea(), 6.25, 0.0001);
+    }
 }
 
 
