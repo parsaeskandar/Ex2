2b63c1408fb93e13a26217c0caefebca003328a3
Kim <yazdanikimia@yahoo.com>
2021-03-17 23:20:51 +0330

    added getHeight and setHeight in square class

--- src/Square.java
+++ src/Square.java
@@ -8,5 +8,13 @@ public class Square implements Shape {
     public double computeArea() {
         return this.height * this.height;
     }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height > 0 ? height : 0;
+    }
     
 }
